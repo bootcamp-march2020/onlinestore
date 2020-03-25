1605e2451d04b62e98fdcd0812719a84c84ad8aa
schandrika-at-791678585244 <surya.c@thoughtworks.com>
2020-03-25 10:17:22 +0530

    [Alex/Surya]: Adding Git project repo

---
+++ src/test/java/com/movie/onlinestore/OnlinestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.onlinestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
